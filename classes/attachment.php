b1a3cee52999fd0ce2ca3616beb3275e8345a9e0
Joey Kamsteeg <joey_kamsteeg@live.nl>
2015-08-10 13:18:10 +0200

    optimalisation

--- classes/attachment.php
+++ classes/attachment.php
@@ -21,8 +21,8 @@
         }
         
         private function load(){
-            $this->credit_name = $this->getMetaData('credit_name'); $this->credit_name = $this->credit_name[0];
-            $this->credit_url = $this->getMetaData('credit_url'); $this->credit_url = $this->credit_url[0];
+            $this->credit_name = $this->getMetaData('credit_name');
+            $this->credit_url = $this->getMetaData('credit_url');
         }
         
         public function save(){
