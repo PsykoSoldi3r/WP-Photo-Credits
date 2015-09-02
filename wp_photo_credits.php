f91d7fcc2745f7402b9f030ac415552ed690174e
Joey Kamsteeg <joey_kamsteeg@live.nl>
2015-09-02 23:08:33 +0200

    get credits info for custom themes

--- wp_photo_credits.php
+++ wp_photo_credits.php
@@ -24,6 +24,7 @@
             $this->createSettings();
             
             add_action('get_image_tag', array( $this, 'imageTag' ), 0, 4 );
+            add_action('PhotoCreditsGetInfo', array( $this,'getCreditsInfo') );
         }
         
         private function createForm(){
@@ -54,6 +55,33 @@
                    $html.
                    $credit_holder.$credit.$credit_holder_close.
                    "</div>";
+        }
+        
+        public function getCreditsInfo( $post_id ){
+            $attachment = new Attachment( $post_id );
+            $language = new Language();
+            
+            $url = $attachment->credit_url;
+            $http = substr($url, 0,7);
+            if( $http != false && $http != "http://"){
+                $url = "http://".$url;
+            }
+            
+            if( (strlen( $attachment->credit_name )) > 0 && ( strlen( $attachment->credit_url ) > 0 ) ){
+                $template = 
+                        "<div class='wpPhotoCredits'>
+                            <a href='{$url}' target='_blank' class='wpPhotoCreditsLink'><div class='wpPhotoCreditsTitle'>{$language->getText('credit_name_post')} {$attachment->credit_name}</div></a>
+                         </div>";
+
+                echo $template;
+            }else if( strlen( $attachment->credit_name ) > 0 ){
+                $template = 
+                        "<div class='wpPhotoCredits'>
+                            <div class='wpPhotoCreditsTitle'>{$language->getText('credit_name_post')} {$attachment->credit_name}</div>
+                         </div>";
+
+                echo $template;
+            }
         }
     }
     
